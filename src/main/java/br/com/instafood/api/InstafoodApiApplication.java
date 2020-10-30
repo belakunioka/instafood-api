4274d09011274c02b7e39798f650069a8911175c
Isabela Kunioka <isanakatani@hotmail.com>
2020-10-30 09:09:28 -0300

    first commit

---
+++ src/main/java/br/com/instafood/api/InstafoodApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.instafood.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InstafoodApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InstafoodApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/instafood/api/InstafoodApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.instafood.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InstafoodApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
