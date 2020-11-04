139696a97135758950ab9a22ed48e34b7d2e2116
belakunioka <65040373+belakunioka@users.noreply.github.com>
2020-11-04 12:09:28 -0300

    Update Utensilio.java

--- src/main/java/br/com/instafood/api/model/Utensilio.java
+++ src/main/java/br/com/instafood/api/model/Utensilio.java
@@ -1,35 +1,27 @@
 package br.com.instafood.api.model;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
 @NoArgsConstructor
 public class Utensilio {
 
-    @Id
+    @Id @Column(name = "id") 
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int idUtensilio;
-
-    private String nome;
-
+    @Getter private int idUtensilio;
+    
+    @Getter @Setter private String nome;
+    
     public Utensilio(String nome) {
         this.nome = nome;
     }
+}
 
-    public String getIdUtensilio(){
-        return this.idReceita;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getNome(){
-        return this.nome;
-    }
-} 
