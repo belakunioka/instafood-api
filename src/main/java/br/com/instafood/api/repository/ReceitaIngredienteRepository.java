142b811b9f997e406dd53c21032cec4888bf758b
Rayssa Ferreira <rayssaferr98@gmail.com>
2020-11-10 02:49:06 -0300

    Criei os repositories de Ingrediente e ReceitaIngrediente

---
+++ src/main/java/br/com/instafood/api/repository/ReceitaIngredienteRepository.java
@@ -0,0 +1,9 @@
+package br.com.instafood.api.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import br.com.instafood.api.model.ReceitaIngrediente;
+
+public interface ReceitaIngredienteRepository extends CrudRepository<ReceitaIngrediente, Integer> {
+
+}
